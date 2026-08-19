2d7e25a3a3cebde06dcbe279f70ffe4ece1d1d8b
Win-k1 <2030002869@qq.com>
2026-08-19 23:00:36 +0800

    day8

--- mem.h
+++ mem.h
@@ -1,19 +1,22 @@
 #ifndef MEM_H
 #define MEM_H
-
 #include <stdlib.h>
+#include <stdio.h>
 
 #define POOL_SIZE 4096
 
 typedef struct BlockHeader
 {
-    size_t size;
+    size_t size;    
     struct BlockHeader *next;
 } BlockHeader;
 
 void mem_init();
+void *mem_alloc(size_t size);
+void mem_free(void *ptr);
+void mem_show(void);
+
 extern void *g_mem_pool;
 extern BlockHeader *g_free_list;
-void mem_free(void *ptr);
 
 #endif
