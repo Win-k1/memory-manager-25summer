4cb7515fb11a73f0d8f474bf9e8838bf1195f51f
Win-k1 <2030002869@qq.com>
2026-08-19 22:59:42 +0800

    day8

--- mem.c
+++ mem.c
@@ -1,32 +1,51 @@
 #include "mem.h"
+#include <stdlib.h>
 
 void mem_init()
 {
-    g_mem_pool = malloc(POOL_SIZE);
-    BlockHeader *block = (BlockHeader *)g_mem_pool;
-    block->size = POOL_SIZE - sizeof(BlockHeader);
-    block->next = NULL;
-    g_free_list = block;
+    g_mem_pool 
+= malloc(POOL_SIZE);
+    BlockHeader 
+*block = (BlockHeader *)g_mem_pool;
+    block
+->size = POOL_SIZE - sizeof(BlockHeader);
+    block
+->next = NULL;
+    g_free_list 
+= block;
 }
+
 void *mem_alloc (size_t size) {
-BlockHeader current = g_free_list, prev = NULL;
-while (current) {
-if (current->size >= size) {
-if (current->size - size > sizeof(BlockHeader)) {
-BlockHeader split = (BlockHeader)((char)current + sizeof(BlockHeader) + size);
-split->size = current->size - size - sizeof(BlockHeader);
-split->next = current->next;
-current->size = size;
-current->next = split;
-}
-if (!prev) g_free_list = current->next;
-else prev->next = current->next;
-return (char)current + sizeof(BlockHeader);
-}
-prev = current;
-current = current->next;
-}
-return NULL;
+    BlockHeader 
+*current = g_free_list, *prev = NULL;
+    while (current) {
+        if (current->size >= size) {
+            if (current->size - size > sizeof(BlockHeader)) {
+                BlockHeader 
+*split = (BlockHeader*)((char*)current + sizeof(BlockHeader) + size);
+                split
+->size = current->size - size - sizeof(BlockHeader);
+                split
+->next = current->next;
+                current
+->size = size;
+                current
+->next = split;
+            }
+            if (!prev) 
+                g_free_list 
+= current->next;
+            else 
+                prev
+->next = current->next;
+            return (char*)current + sizeof(BlockHeader);
+        }
+        prev 
+= current;
+        current 
+= current->next;
+    }
+    return NULL;
 }
 
 void mem_free(void *ptr)
@@ -36,28 +55,37 @@ void mem_free(void *ptr)
         return;
     }
     // 获取内存块头部
-    BlockHeader *free_blk = (BlockHeader*)((char*)ptr - sizeof(BlockHeader));
+    BlockHeader 
+*free_blk = (BlockHeader*)((char*)ptr - sizeof(BlockHeader));
 
-    BlockHeader *cur = g_free_list;
-    BlockHeader *pre = NULL;
+    BlockHeader 
+*cur = g_free_list;
+    BlockHeader 
+*pre = NULL;
 
     // 按地址顺序，找到插入位置
     while(cur != NULL && cur < free_blk)
     {
-        pre = cur;
-        cur = cur->next;
+        pre 
+= cur;
+        cur 
+= cur->next;
     }
 
     // 把释放的块插入空闲链表
     if(pre == NULL)
     {
-        free_blk->next = g_free_list;
-        g_free_list = free_blk;
+        free_blk
+->next = g_free_list;
+        g_free_list 
+= free_blk;
     }
     else
     {
-        free_blk->next = pre->next;
-        pre->next = free_blk;
+        free_blk
+->next = pre->next;
+        pre
+->next = free_blk;
     }
 
     // 和后面相邻块合并
@@ -66,8 +94,10 @@ void mem_free(void *ptr)
         char *end = (char*)free_blk + sizeof(BlockHeader) + free_blk->size;
         if(end == (char*)free_blk->next)
         {
-            free_blk->size += sizeof(BlockHeader) + free_blk->next->size;
-            free_blk->next = free_blk->next->next;
+            free_blk
+->size += sizeof(BlockHeader) + free_blk->next->size;
+            free_blk
+->next = free_blk->next->next;
         }
     }
 
@@ -77,8 +107,24 @@ void mem_free(void *ptr)
         char *pre_end = (char*)pre + sizeof(BlockHeader) + pre->size;
         if(pre_end == (char*)free_blk)
         {
-            pre->size += sizeof(BlockHeader) + free_blk->size;
-            pre->next = free_blk->next;
+            pre
+->size += sizeof(BlockHeader) + free_blk->size;
+            pre
+->next = free_blk->next;
         }
+    }
+}
+
+//新增：打印空闲块mem_show
+void mem_show(void)
+{
+    BlockHeader 
+*p = g_free_list;
+    printf("====空闲内存块信息====\n");
+    while(p != NULL)
+    {
+        printf("块地址:%p, 块大小:%zu\n", p, p->size);
+        p 
+= p->next;
     }
 }
